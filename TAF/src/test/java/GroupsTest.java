a331b63bb7419daf89db9015a8d61fcf7fd0e9b6
KatG17 <katsiaryna.hamarnik@gmail.com>
2022-09-30 21:01:32 +0300

    Maven homework

--- TAF/src/test/java/GroupsTest.java
+++ TAF/src/test/java/GroupsTest.java
@@ -1,6 +1,9 @@
 import org.testng.annotations.Test;
 
 public class GroupsTest extends BaseTest{
+    int a = 6;
+    int b = 3;
+    int expectedResult = 2;
     @Test (groups = "smoke")
     public void stepb() {System.out.println("stepb...");}
 
