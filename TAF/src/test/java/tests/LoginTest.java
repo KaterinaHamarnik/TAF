792b23f8e625993a07b4b4853b4eda75afd45fa4
KatG17 <katsiaryna.hamarnik@gmail.com>
2022-11-08 21:10:45 +0300

    done

--- TAF/src/test/java/tests/LoginTest.java
+++ TAF/src/test/java/tests/LoginTest.java
@@ -20,19 +20,19 @@ import steps.LoginStep;
 public class LoginTest extends BaseTest {
 
 
-    @Test
-    public void successLoginTest() {
-        loginStep.login(ReadProperties.username(), ReadProperties.password());
-
-        Assert.assertTrue(new DashboardPage(driver).isPageOpened());
-    }
-
-    @Test
-    public void successLoginTest1() {
-        Assert.assertTrue(
-                loginStep.loginSuccessful(ReadProperties.username(), ReadProperties.password())
-                        .isPageOpened());
-    }
+//    @Test
+//    public void successLoginTest() {
+//        loginStep.login(ReadProperties.username(), ReadProperties.password());
+//
+//        Assert.assertTrue(new DashboardPage(driver).isPageOpened());
+//    }
+//
+//    @Test
+//    public void successLoginTest1() {
+//        Assert.assertTrue(
+//                loginStep.loginSuccessful(ReadProperties.username(), ReadProperties.password())
+//                        .isPageOpened());
+//    }
 
     @Test
     public void incorrectUsernameTest() {
@@ -42,11 +42,11 @@ public class LoginTest extends BaseTest {
                 , "Email/Login or Password is incorrect. Please try again.");
     }
 
-    @Test
-    public void fail_incorrectUsernameTest() {
-        Assert.assertEquals(
-                loginStep.loginIncorrect("sdsd", ReadProperties.password())
-                        .getErrorTextElement().getText()
-                , "Email/Login or Password is incorrect. Please try again.111");
-    }
+//    @Test
+//    public void fail_incorrectUsernameTest() {
+//        Assert.assertEquals(
+//                loginStep.loginIncorrect("sdsd", ReadProperties.password())
+//                        .getErrorTextElement().getText()
+//                , "Email/Login or Password is incorrect. Please try again.111");
+//    }
 }
